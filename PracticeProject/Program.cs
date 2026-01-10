a0671c5692cefc0043bcafb8303f73e6c59484ac
mimiarabska <mimi_arabska@abv.bg>
2026-01-10 13:48:12 +0200

    Added service builder configuration

--- PracticeProject/Program.cs
+++ PracticeProject/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PracticeProject.Data;
+using PracticeProject.Services.Projects;
 
 namespace PracticeProject
 {
@@ -15,6 +16,7 @@ namespace PracticeProject
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            builder.Services.AddScoped<IProjectService, ProjectService>();
 
             builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>()
